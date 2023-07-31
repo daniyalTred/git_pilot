0bbe479815c2a4ae0e7dffb55008dcc85ee92ad6
Daniyal Anwar <daniyal.anwar@tredence.com>
2023-07-31 10:27:28 +0530

    This is my commit

--- code.py
+++ code.py
@@ -6,10 +6,9 @@ def fetch_dates(start_date, total_days):
     for day in range(1, total_days+1):
         date = dt.strptime(start_date, '%d-%m-%Y') + timedelta(days=day)
         total_dates.append(date.strftime('%d-%m-%Y'))
-        
     return total_dates
 
 date = input("Enter the starting date: ")
 total_days = int(input("Enter the days to be added: "))
 total_dates = fetch_dates(date, total_days)
-print(total_dates)
+print(total_dates);
