abbc43766f09fb5f0815d9fd622491adb25d41e6
Daniyal Anwar <daniyal.anwar@tredence.com>
2023-08-02 12:37:21 +0530

    FET: Added status code

--- gmart_api.py
+++ gmart_api.py
@@ -4,3 +4,4 @@ res=requests.get('https://globalmart-api.onrender.com/mentorskool/v1/sales',
              headers={'accept':'application/json','access_token':'fe66583bfe5185048c66571293e0d358'}
              )
 print(f"the response is: {json.loads(res.text)}")
+print(f"the status code is: {res.status_code}")
